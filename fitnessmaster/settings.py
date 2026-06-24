03ff195d91774ab107027dc6c56f8bf10b52f74e
Ghano <you@example.com>
2026-06-24 11:48:00 +0530

    Fix: Add DEFAULT_AUTO_FIELD to resolve all 15 models.W042 warnings

--- fitnessmaster/settings.py
+++ fitnessmaster/settings.py
@@ -32,6 +32,10 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # Application definition
 
 INSTALLED_APPS = [
